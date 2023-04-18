d0cbde82cdeb24c2ad71f387e3aa49ef168955ee
reharbi <reemharbi.3@gmail.com>
2023-04-18 10:39:09 +0300

    added background animations + reset form button

--- src/App.jsx
+++ src/App.jsx
@@ -33,9 +33,21 @@ function App() {
 
   return (
     <main className="bg-gradient-radial from-cyan-800  to-slate-700 min-h-screen w-screen flex items-center justify-center flex-col">
+      <div className="objects absolute top-0 left-0 w-full h-full overflow-hidden">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
       <h1 className="text-white mb-28 font-bold text-3xl sm:text-5xl lg:text-6xl">QR Code Generator</h1>
-      <div className="container mx-aut">
-        <form onSubmit={(e) => generateImage(e)} className="bg-slate-200 rounded-lg p-5 sm:p-10 lg:p-20 mx-5">
+      <div className="container mx-auto relative z-10">
+        <form onSubmit={(e) => generateImage(e)} className="bg-slate-200 shadow-lg rounded-lg p-5 sm:p-10 lg:p-20 mx-5">
           <div className="mt-2">
             <input
               type="text"
@@ -46,17 +58,26 @@ function App() {
               placeholder="Enter a valid url starting with http(s)"
               className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-cyan-600 sm:text-sm sm:leading-6"
             />
+            <div className="flex justify-end">
+              <input
+                type="reset"
+                value="clear"
+                className="cursor-pointer hover:opacity-80 pr-2 mt-1"
+              />
+            </div>
           </div>
-          <button
-            type="submit"
-            className="bg-cyan-800 hover:bg-cyan-600 text-white font-semibold py-2 px-4 rounded-md mt-5"
-          >
-            Generate
-          </button>
+          <div className="flex items-center justify-center">
+            <button
+              type="submit"
+              className="bg-cyan-800 text-xl hover:bg-cyan-600 text-white font-semibold py-2 px-4 rounded-md mt-5"
+            >
+              Generate
+            </button>
+          </div>
         </form>
       </div>
       {show ? (
-        <div className="modal absolute backdrop-blur-lg bg-white/30 h-5/6 lg:h-2/4 w-11/12 lg:w-1/3 rounded-lg">
+        <div className="modal absolute z-20 backdrop-blur-lg bg-white/30 h-5/6 lg:h-2/4 w-11/12 lg:w-1/3 rounded-lg">
           <div className="flex items-center justify-center flex-col relative h-full">
             <XMarkIcon
               className="w-8 absolute top-5 left-5 text-white hover:scale-125 cursor-pointer transition ease-in-out duration-150"
@@ -68,7 +89,7 @@ function App() {
             <a
               href={img}
               download="qrcode.png"
-              className="bg-cyan-800 hover:bg-cyan-600 text-white font-semibold py-2 px-4 rounded-md mt-12"
+              className="bg-cyan-800 text-xl hover:bg-cyan-600 text-white font-semibold py-2 px-4 rounded-md mt-12"
             >
               Download
             </a>
